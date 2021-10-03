0e7982a3a70d6f80e2fe0abcf8971681a9d1d18d
JustAnyone <dom.svetikas@gmail.com>
2021-10-03 21:41:32 +0300

    Add support for log_channel, strict_anti_phising, suspicious_users and banned_exact_words configuration attributes

--- bot/cogs/models/configuration.py
+++ bot/cogs/models/configuration.py
@@ -4,19 +4,11 @@ from bson.int64 import Int64
 from bson.objectid import ObjectId
 from discord.channel import TextChannel
 from discord.role import Role
-from enum import Enum
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
     from client import Pidroid
     from cogs.utils.api import API
-
-
-class ConfigKeys(Enum):
-    jail_role = "jail_role"
-    jail_channel = "jail_channel"
-
-    mute_role = "mute_role"
 
 
 class GuildConfiguration:
@@ -24,12 +16,16 @@ class GuildConfiguration:
     if TYPE_CHECKING:
         _id: ObjectId
         guild_id: int
-
         jail_channel: int
         jail_role: int
-
         mute_role: int
+        log_channel: int
+
+        strict_anti_phising: bool
         public_tags: bool
+
+        suspicious_users: List[str]
+        banned_exact_words: List[str]
 
     def __init__(self, api: API, data: dict) -> None:
         self.api = api
@@ -41,17 +37,27 @@ class GuildConfiguration:
         self.guild_id = c["guild_id"]
 
         self.prefixes = c.get("prefixes", [])
+
+        self.jail_channel = c.get("jail_role", None)
+        self.jail_role = c.get("jail_role", None)
+        self.mute_role = c.get("mute_role", None)
+        self.log_channel = c.get("log_channel", None)
+
         self.public_tags = c.get("public_tags", False)
+        self.strict_anti_phising = c.get("strict_anti_phising", False)
 
-        self.jail_channel = c.get(ConfigKeys.jail_channel.value, None)
-        self.jail_role = c.get(ConfigKeys.jail_role.value, None)
-
-        self.mute_role = c.get(ConfigKeys.mute_role.value, None)
+        self.suspicious_users = c.get("suspicious_users", [])
+        self.banned_exact_words = c.get("banned_exact_words", [])
 
     async def update_public_tag_permission(self, allow_public: bool) -> None:
         """Updates public tag permission."""
         self.public_tags = allow_public
         await self.api.set_guild_config(self._id, "public_tags", allow_public)
+
+    async def update_anti_phising_permission(self, strict: bool) -> None:
+        """Updates strict anti-phising permission."""
+        self.strict_anti_phising = strict
+        await self.api.set_guild_config(self._id, "strict_anti_phising", strict)
 
     async def update_prefix(self, prefix: str) -> None:
         """Updates the guild bot prefix."""
@@ -61,26 +67,40 @@ class GuildConfiguration:
     async def update_jail_channel(self, channel: TextChannel) -> None:
         """Updates the guild jail text channel."""
         self.jail_channel = channel.id
-        key = ConfigKeys.jail_channel.value
         if channel is None:
-            return await self.api.unset_guild_config(self._id, key)
-        await self.api.set_guild_config(self._id, key, Int64(channel.id))
+            return await self.api.unset_guild_config(self._id, "jail_channel")
+        await self.api.set_guild_config(self._id, "jail_channel", Int64(channel.id))
 
     async def update_jail_role(self, role: Role) -> None:
         """Updates the guild jail role."""
         self.jail_role = role.id
-        key = ConfigKeys.jail_role.value
         if role is None:
-            return await self.api.unset_guild_config(self._id, key)
-        await self.api.set_guild_config(self._id, key, Int64(role.id))
+            return await self.api.unset_guild_config(self._id, "jail_role")
+        await self.api.set_guild_config(self._id, "jail_role", Int64(role.id))
 
     async def update_mute_role(self, role: Role) -> None:
         """Updates the guild mute role."""
         self.mute_role = role.id
-        key = ConfigKeys.mute_role.value
         if role is None:
-            return await self.api.unset_guild_config(self._id, key)
-        await self.api.set_guild_config(self._id, key, Int64(role.id))
+            return await self.api.unset_guild_config(self._id, "mute_role")
+        await self.api.set_guild_config(self._id, "mute_role", Int64(role.id))
+
+    async def update_log_channel(self, channel: TextChannel) -> None:
+        """Updates the guild log text channel."""
+        self.log_channel = channel.id
+        if channel is None:
+            return await self.api.unset_guild_config(self._id, "log_channel")
+        await self.api.set_guild_config(self._id, "log_channel", Int64(channel.id))
+
+    async def update_suspicious_users(self, suspicious_users: List[str]) -> None:
+        """Updates the suspicious user list."""
+        self.suspicious_users = suspicious_users
+        await self.api.set_guild_config(self._id, "suspicious_users", suspicious_users)
+
+    async def update_banned_exact_words(self, banned_words: List[str]) -> None:
+        """Updates the exact banned words list."""
+        self.banned_exact_words = banned_words
+        await self.api.set_guild_config(self._id, "banned_exact_words", banned_words)
 
 
 def setup(client: Pidroid):
