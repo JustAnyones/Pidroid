738035694aa5acedc83a562bf8b32e1379ab35b4
JustAnyone <dom.svetikas@gmail.com>
2022-01-04 14:10:42 +0200

    Make get_channel more compliant with its calls

--- bot/cogs/utils/getters.py
+++ bot/cogs/utils/getters.py
@@ -7,7 +7,9 @@ def get_role(guild: discord.Guild, role_id: Optional[int]) -> Optional[discord.R
         return None
     return guild.get_role(role_id)
 
-def get_channel(guild: discord.Guild, channel_id: int) -> Optional[discord.abc.GuildChannel]:
+def get_channel(guild: discord.Guild, channel_id: Optional[int]) -> Optional[discord.abc.GuildChannel]:
+    if channel_id is None:
+        return None
     return guild.get_channel(channel_id)
 
 
