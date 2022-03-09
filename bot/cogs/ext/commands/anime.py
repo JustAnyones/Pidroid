59fad5b589843c87c07150b28d0730b08c133dbf
erik <eriksmit18@gmail.com>
2022-03-09 18:17:39 +0100

    ensure selection is always at least 2 characters long

--- bot/cogs/ext/commands/anime.py
+++ bot/cogs/ext/commands/anime.py
@@ -191,6 +191,9 @@ class AnimeCommands(commands.Cog):
     @commands.max_concurrency(number=1, per=commands.BucketType.user)
     async def waifu(self, ctx: Context, *, selection: str = None):
         async with ctx.typing():
+            if len(selection) < 2:
+                return await ctx.reply(embed=error("Your selection must be at least 2 characters long!"))
+
             api = self.waifu_list_api
             waifus = []
 
