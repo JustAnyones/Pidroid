ccb24a4e59388e6a4f70fe2af7281bf8fb128f2b
JustAnyone <dom.svetikas@gmail.com>
2022-03-30 22:02:48 +0300

    Improve translation system

--- bot/cogs/ext/events/translator.py
+++ bot/cogs/ext/events/translator.py
@@ -1,14 +1,14 @@
 import asyncio
+import base64
 import emoji # I am not updating the emoji regex myself every time there's a new one
 import re
 
 from contextlib import suppress
-from discord.embeds import Embed
 from discord.ext import commands
 from discord.channel import TextChannel
 from discord.utils import remove_markdown
 from discord.message import Message
-from typing import List
+from typing import List, Optional, Tuple
 
 from client import Pidroid
 from cogs.utils.checks import is_client_pidroid
@@ -48,11 +48,68 @@ FEED_CHANNEL = 943920969637040140
 SOURCE_CHANNEL = 692830641728782336
 
 CUSTOM_EMOJI_PATTERN = re.compile(r'<(a:.+?:\d+|:.+?:\d+)>')
+URL_PATTERN = re.compile(r'(https?:\/\/)(\s)*(www\.)?(\s)*((\w|\s)+\.)*([\w\-\s]+\/)*([\w\-]+)((\?)?[\w\s]*=\s*[\w\%&]*)*')
+BASE64_PATTERN = re.compile(r'^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$')
 
 def remove_emojis(string: str) -> str:
     """Removes all emojis from a string."""
     stripped = re.sub(CUSTOM_EMOJI_PATTERN, "", string)
     return emoji.get_emoji_regexp().sub("", stripped)
+
+def remove_urls(string: str) -> str:
+    """Removes URLs from a string."""
+    return re.sub(URL_PATTERN, "", string)
+
+class ParserFlags:
+    NORMAL     = 1 << 0 # noqa
+    LOWERCASED = 1 << 1
+    BASE64     = 1 << 2 # noqa
+    BYPASSED   = 1 << 3 # noqa
+    FAIL       = 1 << 4 # noqa
+
+
+FLAG_FOOTERS = {
+    ParserFlags.LOWERCASED: "Text has been lowercased",
+    ParserFlags.BASE64: "Text has been converted from base64",
+    ParserFlags.FAIL: "Failed parsing a base64 string"
+}
+
+class TextParser:
+    def __init__(self, text: str, remove_markdown: bool = True) -> None:
+        self.original = text
+        self.remove_markdown = remove_markdown
+
+    @property
+    def text(self) -> str:
+        if self.remove_markdown:
+            return remove_markdown(self.original).strip()
+        return self.original
+
+    @property
+    def stripped_text(self) -> str:
+        string = remove_emojis(self.text).strip()
+        return remove_urls(string).strip()
+
+    @property
+    def should_translate(self) -> bool:
+        return len(self.stripped_text) > 0
+
+    def get_parsed_text(self) -> Tuple[int, Optional[str]]:
+        # If string is base64
+        if re.match(BASE64_PATTERN, self.text):
+            try:
+                decoded_string = base64.b64decode(self.text).decode("utf-8")
+            except UnicodeDecodeError:
+                return ParserFlags.BASE64, None
+            return ParserFlags.BASE64, decoded_string
+
+        # If 30% of all characters are uppercase, lowercase the entire string
+        #print("Value of capitalized letters in string", sum(1 for c in self.text if c.isupper()) / len(self.text))
+        if not self.text.isupper() and sum(1 for c in self.text if c.isupper()) / len(self.text) >= 0.30:
+            return ParserFlags.LOWERCASED, self.text.lower()
+
+        # Otherwise, just return normal string
+        return ParserFlags.NORMAL, self.text
 
 class TranslationEventHandler(commands.Cog):
     """This class implements a cog for event handling related to TheoTown translations."""
@@ -103,7 +160,7 @@ class TranslationEventHandler(commands.Cog):
             and message.channel.id == SOURCE_CHANNEL
         )
 
-    async def translate_message(self, message: Message, clean_text: str) -> List[Embed]:
+    async def translate_message(self, message: Message, clean_text: str) -> List[dict]:
         # Await previous translation jobs to finish
         await self._translating.wait()
 
@@ -124,18 +181,10 @@ class TranslationEventHandler(commands.Cog):
         if len(translations) == 0:
             self.client.logger.warning(f"Unable to translate '{message.clean_content}'")
 
-        # Create a list of embeds
-        embeds = []
-        for translation in translations:
-            text: str = translation["text"]
-            detected_lang: str = translation["detected_source_language"]
-            embed = PidroidEmbed(description=text)
-            embed.set_footer(text=f"Detected source language: {LANGUAGE_MAPPING.get(detected_lang, detected_lang)}")
-            embeds.append(embed)
-        return embeds
+        return translations
 
     @commands.Cog.listener()
-    async def on_message(self, message: Message): # noqa C901
+    async def on_message(self, message: Message):
         # Check whether message is valid for further processing
         if not self.is_valid(message):
             return
@@ -148,15 +197,22 @@ class TranslationEventHandler(commands.Cog):
         if utcnow().timestamp() - self.last_reset.timestamp() > 60 * 60 * 24:
             self.used_chars = 0
             self.last_reset = utcnow()
+        await self.handle(message)
 
-        raw_text = remove_markdown(message.clean_content).strip()
+    async def handle(self, message: Message):
+        parser = TextParser(message.clean_content)
+        translations = []
+        flag = ParserFlags.BYPASSED
+        if parser.should_translate:
+            flag, text = parser.get_parsed_text()
+            if text is None and flag == ParserFlags.FAIL:
+                self.client.logger.warn(f"Failed parsing of base64 text for '{parser.text}'")
+                text = parser.text
+                flag = ParserFlags.FAIL
+            translations = await self.translate_message(message, text)
+        await self.dispatch_translation(message, translations, flag)
 
-        # If there's extra content without emojis, translate the whole text
-        if len(remove_emojis(raw_text)) != 0:
-            embeds = await self.translate_message(message, raw_text)
-        else:
-            embeds = [PidroidEmbed(description=raw_text)]
-
+    async def dispatch_translation(self, message: Message, translations: List[dict], flag: int) -> None: # noqa C901
         # If message contains a reply, track down the reference author
         action = None
         if message.reference:
@@ -170,6 +226,17 @@ class TranslationEventHandler(commands.Cog):
             rich_description += f"[Sticker {sticker.name}]\n"
 
         attachments = [a.url for a in message.attachments]
+
+        if flag == ParserFlags.BYPASSED:
+            embeds = [PidroidEmbed(description=message.clean_content)]
+        else:
+            embeds = []
+            for translation in translations:
+                text: str = translation["text"]
+                detected_lang: str = translation["detected_source_language"]
+                embed = PidroidEmbed(description=text)
+                embed.set_footer(text=f"Detected source language: {LANGUAGE_MAPPING.get(detected_lang, detected_lang)}")
+                embeds.append(embed)
 
         # Go over each embed and set author values or other stuff
         for embed in embeds:
@@ -187,9 +254,13 @@ class TranslationEventHandler(commands.Cog):
             if len(attachments) > 0:
                 embed.add_field(name="Attachments", value='\n'.join(attachments), inline=False)
 
-            # If it wasn't translated, notify the moderator
-            if embed.footer.text == Embed.Empty:
+            # Notify of any modifications done to the original string
+            if flag == ParserFlags.BYPASSED:
                 embed.set_footer(text="Translation layer bypassed as nothing translatable was found")
+            else:
+                footer = FLAG_FOOTERS.get(flag)
+                if footer is not None:
+                    embed.set_footer(text=f"{footer} | {embed.footer.text}")
 
         await self.channel.send(embeds=embeds)
 
