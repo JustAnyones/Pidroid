609ad19ea2847900181cba4631d9686b3f363a53
erik <eriksmit18@gmail.com>
2022-06-02 19:55:13 +0200

    add auto_archive_duration as optional parameter

--- bot/client.py
+++ bot/client.py
@@ -183,10 +183,11 @@ class Pidroid(commands.Bot):
     def _remove_guild_configuration(self, guild_id: int) -> None:
         self.guild_configurations.pop(guild_id)
 
-    async def create_expiring_thread(self, message: Message, name: str, expire_timestamp: int):
+    async def create_expiring_thread(self, message: Message, name: str, expire_timestamp: int, auto_archive_duration=60):
         """Creates a new expiring thread"""
-        thread = await message.create_thread(name=name, auto_archive_duration=60)
+        thread = await message.create_thread(name, auto_archive_duration)
         await self.api.create_new_expiring_thread(thread.id, expire_timestamp)
+        return thread
 
     async def dispatch_log(self, guild: Guild, log: BaseLog):
         """Dispatches a Pidroid log to a guild channel, if applicable."""
