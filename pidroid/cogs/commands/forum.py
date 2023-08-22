cbd819a71e815a96425cba0af457618f34c5f4b2
JustAnyone <dom.svetikas@gmail.com>
2023-08-22 18:34:45 +0300

    Update forum commands

--- pidroid/cogs/commands/forum.py
+++ pidroid/cogs/commands/forum.py
@@ -1,9 +1,6 @@
-import inspect
-import re
-
 from discord.ext import commands
-from discord.ext.commands import MissingRequiredArgument, BadArgument, Parameter # type: ignore
-from discord.ext.commands.context import Context # type: ignore
+from discord.ext.commands import MissingRequiredArgument, BadArgument
+from discord.ext.commands.context import Context
 from discord.utils import format_dt
 
 from pidroid.client import Pidroid
@@ -11,24 +8,24 @@ from pidroid.cogs.handlers.error_handler import notify
 from pidroid.models.accounts import ForumAccount
 from pidroid.models.categories import TheoTownCategory
 from pidroid.utils.decorators import command_checks
-from pidroid.utils.embeds import PidroidEmbed, ErrorEmbed, SuccessEmbed
+from pidroid.utils.embeds import PidroidEmbed, SuccessEmbed
 from pidroid.utils.http import Route
 
-class ForumCommands(commands.Cog): # type: ignore
+class ForumCommands(commands.Cog):
     """This class implements a cog which contains commands for communication via TheoTown forums."""
 
     def __init__(self, client: Pidroid):
         self.client = client
 
-    @commands.command( # type: ignore
-        name='forum-user',
+    @commands.command(
+        name='forum-account',
         brief='Returns forum account information for the specified user.',
         usage='<forum username/ID>',
-        aliases=['forum-ui'],
+        aliases=['forum-ui', 'forum-user'],
         category=TheoTownCategory
     )
-    @commands.bot_has_permissions(send_messages=True) # type: ignore
-    async def forum_user_command(self, ctx: Context, username: str):
+    @commands.bot_has_permissions(send_messages=True)
+    async def forum_account_command(self, ctx: Context, username: str):
         async with ctx.typing():
             res = await self.client.api.get(Route(
                 "/private/forum/find_account",
@@ -56,83 +53,66 @@ class ForumCommands(commands.Cog): # type: ignore
             embed.set_thumbnail(url=account.avatar_url)
             await ctx.reply(embed=embed)
 
-    @forum_user_command.error
-    async def on_forum_user_command_error(self, ctx: Context, error):
+    @forum_account_command.error
+    async def on_forum_account_command_error(self, ctx: Context, error):
         if isinstance(error, MissingRequiredArgument):
             if error.param.name == "username":
                 return await notify(ctx, "Please specify the username to view the information for.")
         setattr(error, 'unhandled', True)
 
-    # TODO: refactor arguments
-    @commands.command( # type: ignore
+    @commands.group(
         name='forum-gift',
-        brief='Gifts specified items to the specified user.',
-        usage='<diamonds/region coins> <amount> <forum username/ID>',
-        permissions=["TheoTown developer"],
+        invoke_without_command=True,
+        hidden=True
+    )
+    async def forum_gift_command(self, ctx: Context):
+        pass
+
+    @forum_gift_command.command(
+        name='diamonds',
+        brief='Gifts specified amount of diamonds to the user.',
+        usage='<forum username/ID> <amount>',
         category=TheoTownCategory
     )
-    @commands.bot_has_permissions(send_messages=True) # type: ignore
+    @commands.bot_has_guild_permissions(send_messages=True)
     @command_checks.is_theotown_developer()
-    async def forum_gift(self, ctx: Context, *args):
-        async with ctx.typing():
-            if len(args) == 0:
-                raise MissingRequiredArgument(Parameter("item", inspect.Parameter.POSITIONAL_OR_KEYWORD, None, None))
+    async def diamonds_subcommand(self, ctx: Context, user: str, amount: int):
+        res = await self.client.api.get(Route(
+            "/private/game/gift",
+            {"username": user, "diamonds": amount}
+        ))
+        if res["success"]:
+            data = res["data"]
+            return await ctx.reply(f'{amount:,} diamonds have been gifted to {data["name"]}!')
+        raise BadArgument(res["details"])
 
-            # Join arguments into a single string
-            command_args = ' '.join(args[0:])
+    @forum_gift_command.command(
+        name='coins',
+        brief='Gifts specified amount of region coins to the user.',
+        usage='<forum username/ID> <amount>',
+        category=TheoTownCategory
+    )
+    @commands.bot_has_guild_permissions(send_messages=True)
+    @command_checks.is_theotown_developer()
+    async def region_coins_subcommand(self, ctx: Context, user: str, amount: int):
+        res = await self.client.api.get(Route(
+            "/private/game/gift",
+            {"username": user, "regioncoins": amount}
+        ))
+        if res["success"]:
+            data = res["data"]
+            return await ctx.reply(f'{amount:,} region coins have been gifted to {data["name"]}!')
+        raise BadArgument(res["details"])
 
-            # Regex to find if valid gift is specified
-            gift_type = re.match('(?i)(?:diamonds)|(?:region coins)', command_args)
-            if not gift_type:
-                await ctx.reply(embed=ErrorEmbed("Invalid gift type specified!"))
-                return
-
-            # Get regex'ed string position in a string as tuple (start, finish)
-            pos = gift_type.regs[0]
-
-            # Convert remaining arguments into list
-            parsed_args = command_args[pos[1]:].strip().split()
-
-            # Raise errors for missing arguments
-            if len(parsed_args) == 0:
-                raise MissingRequiredArgument(Parameter("amount", inspect.Parameter.POSITIONAL_OR_KEYWORD, None, None))
-            if len(parsed_args) == 1:
-                raise MissingRequiredArgument(Parameter("user", inspect.Parameter.POSITIONAL_OR_KEYWORD, None, None))
-
-            # Parse the arguments for API
-            item = command_args[pos[0]:pos[1]].lower()
-            amount = parsed_args[0]
-            try:
-                amount = int(amount) # type: ignore
-            except ValueError:
-                return await ctx.reply(embed=ErrorEmbed(f"{amount} is not a valid amount!"))
-
-            assert isinstance(amount, int)
-            amount = abs(amount)
-            user = ' '.join(parsed_args[1:])
-
-            encoded_item = item.replace(' ', '')
-
-            # Send the request to API
-            res = await self.client.api.get(Route(
-                "/private/game/gift",
-                {"username": user, encoded_item: amount}
-            ))
-            if res["success"]:
-                data = res["data"]
-                return await ctx.reply(f'{amount:,} {item} have been gifted to {data["name"]}!')
-            raise BadArgument(res["details"])
-
-    @commands.command( # type: ignore
-        name='forum-authorise',
+    @commands.command(
+        name='forum-activate',
         brief='Activates specified user\'s forum account.',
         usage='<forum username/ID>',
-        aliases=['forum-auth', 'forum-activate'],
         category=TheoTownCategory
     )
-    @commands.bot_has_permissions(send_messages=True) # type: ignore
+    @commands.bot_has_permissions(send_messages=True)
     @command_checks.is_theotown_developer()
-    async def forum_authorise_command(self, ctx: Context, user: str):
+    async def forum_activate_command(self, ctx: Context, user: str):
         async with ctx.typing():
             res = await self.client.api.get(Route(
                 "/private/forum/activate_account",
@@ -142,8 +122,8 @@ class ForumCommands(commands.Cog): # type: ignore
                 return await ctx.reply(embed=SuccessEmbed(res['details']))
             raise BadArgument(res["details"])
 
-    @forum_authorise_command.error
-    async def on_forum_authorise_command_error(self, ctx: Context, error):
+    @forum_activate_command.error
+    async def on_forum_activate_command_error(self, ctx: Context, error):
         if isinstance(error, MissingRequiredArgument):
             if error.param.name == "username":
                 return await notify(ctx, "Please specify the username of the user you are trying to authorize.")
