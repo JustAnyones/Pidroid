5a09cc3b8ac9b56b94885186a486555dca0b0a99
JustAnyone <dom.svetikas@gmail.com>
2023-08-24 18:06:53 +0300

    Bump to 5.15

--- pidroid/client.py
+++ pidroid/client.py
@@ -38,7 +38,7 @@ class VersionInfo(NamedTuple):
     commit_id: str
 
 
-__VERSION__ = VersionInfo(major=5, minor=14, micro=0, commit_id=os.environ.get('GIT_COMMIT', ''))
+__VERSION__ = VersionInfo(major=5, minor=15, micro=0, commit_id=os.environ.get('GIT_COMMIT', ''))
 
 class Pidroid(commands.Bot):
     """This class represents the Pidroid bot client object."""
