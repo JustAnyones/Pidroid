e75fcebcee6ee8067ef3deaeea44903a7f32df24
JustAnyone <dom.svetikas@gmail.com>
2025-05-25 18:04:47 +0300

    Add back h2 dependency

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker build . --file Dockerfile --build-arg GIT_COMMIT=$(git rev-parse --short HEAD) --tag pidroid
+docker build . --file Dockerfile --build-arg GIT_COMMIT="$(git rev-parse --short HEAD)" --tag pidroid
+docker tag pidroid ghcr.io/justanyones/pidroid
