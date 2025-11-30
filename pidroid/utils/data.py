73db13c607f94b17321ed6f4d35c1e91b90997b5
JustAnyone <dom.svetikas@gmail.com>
2025-11-30 11:58:48 +0200

    Synchronize access to data store

--- pidroid/utils/data.py
+++ pidroid/utils/data.py
@@ -1,9 +1,27 @@
+import asyncio
 import aiodbm
 import os
+
+from contextlib import asynccontextmanager
 
 from pidroid.constants import DATA_FILE_PATH
 
 PERSISTENT_DATA_FILE = os.path.join(DATA_FILE_PATH, "data.dbm")
 
-def PersistentDataStore():
-    return aiodbm.open(PERSISTENT_DATA_FILE, "c") # pyright: ignore[reportUnknownMemberType]
+_DATA_STORE_LOCK = asyncio.Lock()
+
+@asynccontextmanager
+async def PersistentDataStore():
+    """
+    Asynchronously opens the persistent data store, protected by a lock.
+    """
+    await _DATA_STORE_LOCK.acquire()
+    
+    store = None
+    try:
+        store = await aiodbm.open(PERSISTENT_DATA_FILE, "c") # pyright: ignore[reportUnknownMemberType]
+        yield store
+    finally:
+        if store is not None:
+            await store.close()
+        _DATA_STORE_LOCK.release()
